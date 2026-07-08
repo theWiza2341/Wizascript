0eeebf4a094f8ff3cb501c236b72bc2836b882df
theWiza2341 <redstonewiza2341@gmail.com>
2026-07-08 00:51:55 -0500

    Create debug-logger.js

---
+++ packages/core/debug-logger.js
@@ -0,0 +1,3 @@
+// Shared toggleable, prefixed console logging. Needs to support both
+// category flags (e.g. "cow", "draw") and severity levels (log/warn/
+// error), since different scripts have wanted both independently.

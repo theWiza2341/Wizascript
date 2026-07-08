e3eb8b8fc592ae1936e37a824ef61ee9159a1242
theWiza2341 <redstonewiza2341@gmail.com>
2026-07-08 00:50:20 -0500

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,4 @@
+// Entry point for the build. Once packages/* have real code, this file
+// will import each feature's init function and call them in order under
+// a single UnderScript plugin registration. Left empty until core/
+// and the feature packages have something to import.

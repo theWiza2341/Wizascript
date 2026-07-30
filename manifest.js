0146eea7bf55f217983885513088e6797eca578b
theWiza2341 <redstonewiza2341@gmail.com>
2026-07-30 13:18:26 -0500

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -2,6 +2,7 @@ import { bootstrap } from "./packages/core/bootstrap.js";
 import { initPatchMaker } from "./packages/patch-maker/index.js";
 import { initTrueHubBridge } from "./packages/true-hub-bridge/index.js";
 import { initDeckTracker } from "./packages/deck-tracker/index.js";
+import { initUcTv } from "./packages/uc-tv/index.js";
 import { initMisc } from "./packages/misc/index.js";
 
 // NOTE: Doom Reminder (both "Classic" chat-ping and "Evil" clickbait-
